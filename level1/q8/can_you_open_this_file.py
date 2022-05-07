bddc8fa1f3fa64a64d7a29cab9f068f6e7fa4c33
Kali <hiroshi31100@gmail.com>
2022-05-07 19:38:44 -0400

    cant get the flag.. manually open the doc

--- level1/q8/can_you_open_this_file.py
+++ level1/q8/can_you_open_this_file.py
@@ -4,16 +4,13 @@ import subprocess
 
 FILE = 'open_me'
 
-def open_file(file):
+def detect_file(file):
   subprocess.call(f'file %s' % file, shell=True) 
 
   # python-docx cannot handle this file because of too old version.
   if(olefile.isOleFile(file)):
     subprocess.call(f'mv %s %s.doc' % (file, file), shell=True)
       
-  else:
-    return 'CANT FIND FLAG'
 
 if __name__ == '__main__':
-  flag = open_file(FILE)
-  print(flag)
+  detect_file(FILE)
